ca2671dace89a6dae29d83eaaa7851a030c8309d
Telesphore <tuganimana01@gmail.com>
2021-02-11 20:21:45 +0200

    added new

--- src/components/dashboard/Signup.js
+++ src/components/dashboard/Signup.js
@@ -10,7 +10,7 @@ function Signup() {
         <>
 
 <div class="container">
-    <div class="row">
+    <div class="row mt-24">
       <div class="col-lg-10 col-xl-9 mx-auto">
         <div class="card card-signin flex-row my-5">
           <div class="card-img-left d-none d-md-flex">
