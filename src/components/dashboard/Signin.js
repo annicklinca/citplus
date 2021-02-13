e34c4d15d539dd3d3112ac99e49f456e26413185
Telesphore <tuganimana01@gmail.com>
2021-02-13 13:28:51 +0200

    nloggin

--- src/components/dashboard/Signin.js
+++ src/components/dashboard/Signin.js
@@ -1,11 +1,60 @@
 import { Form } from "react-bootstrap";
-import React,{useState,useHistory} from 'react';
+import React,{useState} from 'react';
 import "../../css/tailwindcss.css";
 import "../../css/sign.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Cityplus from '../../images/Cityplus.png';
-
+import axios from 'axios'
+import urlPath from '../../constant'
+import { useHistory } from 'react-router-dom';
 function Signin() {
+
+  const history=useHistory()
+  const [email, setEmail] = useState('')
+  const [password,setPassword]=useState('')
+
+  const [loading,setLoading]=useState(false)
+  const [isError,setIserror]=useState(false)
+  const [message,setMessage]=useState('')
+
+  const handleSignin=(event)=>{
+   
+    event.preventDefault()
+    const data={
+
+      email:email,
+          password:password
+    }
+    setLoading(true)
+    if( email ==='' ||password==='' ){
+      setMessage('Please fill all fields')
+      setIserror(true)
+      setLoading(false)
+    }
+    else{
+    
+ axios.post(`${urlPath.login}`,data)
+        .then((res)=>{
+
+         setMessage(res.data.message)
+         localStorage.setItem('token', res.data.data.token)
+         localStorage.setItem('fullname',res.data.data.fullname)
+         localStorage.setItem('email',res.data.data.email)
+         setTimeout(() => {
+           history.push('/home')
+         }, 1000);
+         setLoading(false)
+         setIserror(true)
+        })
+        .catch((err)=>{
+              setMessage('Invalid username or password')
+          setLoading(false)
+         setIserror(true)
+        })
+     
+    }
+
+  }
     return(
         <>
 
@@ -17,19 +66,26 @@ function Signin() {
             
           </div>
           <div class="card-body">
-            <img src={Cityplus} className="w-20 float-right" />  
+            <img src={Cityplus} className="w-20 float-right" alt="" />  
             <h5 class="card-title text-blue-500">Login Account</h5>
-            
+            { isError?  <div   className="bg-green-200 p-2 text-gray-600  font-bold rounded-xl mb-2">
+              <p className="text-center">{message}</p>
+              </div>
+              :<span></span>}
             <form class="form-signin">
               
               <div class="form-label-group">
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required />
+                <input type="email" value={email} onChange={(e)=>{
+                  setEmail(e.target.value)
+                }} id="inputEmail" class="form-control" placeholder="Email address" required />
                 <label for="inputEmail">Username or Email</label>
               </div>
               
 
               <div class="form-label-group">
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required />
+                <input type="password" value={password} onChange={(e)=>{
+                  setPassword(e.target.value)
+                }} id="inputPassword" class="form-control" placeholder="Password" required />
                 <label for="inputPassword">Password</label>
               </div>
               <div className="from-label-group p-1">
@@ -38,7 +94,8 @@ function Signin() {
               
 
              <div className="from-label-group">
-              <a href="/Home" className="btn btn-lg btn-primary btn-block text-uppercase">sign in</a>
+             {loading? <button className="btn btn-lg btn-primary btn-block text-uppercase">Logging.....</button>:   <button onClick={handleSignin} className="btn btn-lg btn-primary btn-block text-uppercase">Sign In</button>
+ }
             </div> 
              <div className="from-label-group pt-2">
               <p>I don't have an account? 
---
+++ src/constant.js
@@ -0,0 +1,10 @@
+const url ="https://cityplus-ida.herokuapp.com/"
+
+ const urlPath ={
+    login:`${url}login`,
+    signup:`${url}create-account`
+
+
+}
+
+module.exports=urlPath
